e00b38da82da892460a70dce856f57336e5f8427
Natanael da Silva <dasilvanatanael700@gmail.com>
2020-02-25 21:03:23 -0300

    comite com o db Eloquent

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,10 +3,17 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Tarefa;
 
 class HomeController extends Controller
 {
    public function __invoke(){
-       return view('welcome');
+       //return view('welcome');
+    //    $list = Tarefa::all();
+       $list = Tarefa::where('resolvido',0)->get();
+       foreach($list as $item){
+           echo $item->titulo."<br/>";
+       }
+    
    }
 }
--- app/Http/Controllers/TarefasController.php
+++ app/Http/Controllers/TarefasController.php
@@ -4,12 +4,14 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use App\Tarefa;
 
 class TarefasController extends Controller
 {
     public function list(){
         // $list=DB::select('SELECT * FROM tarefas WHERE resolvido = ?',[1]);trazer somente os resolvidos
-        $list=DB::select('SELECT * FROM tarefas');
+        // $list=DB::select('SELECT * FROM tarefas');
+        $list = Tarefa::all();
         
         return view('tarefas.list',[
             'list'=>$list
@@ -26,21 +28,30 @@ class TarefasController extends Controller
             ]);
             $titulo = $request->input('titulo');// pegando o valor e salvando na variavel
 
-            DB::insert('INSERT INTO tarefas (titulo) VALUES (:titulo)',[
-            'titulo'=>$titulo
-            ]);
+            // DB::insert('INSERT INTO tarefas (titulo) VALUES (:titulo)',[
+            // 'titulo'=>$titulo
+            // ]);
 
-            return redirect()->route('tarefas.add')->with('warning','Você não preencheu o titulo');
+            $t = new Tarefa;
+            $t->titulo = $titulo;// titulo vai receber a variavel titulo digitada pelo usuario
+            $t->save();
+
+
+
+            return redirect()->route('tarefas.list');
         
     }
     public function edit($id){
-        $data = DB::select('SELECT * FROM tarefas WHERE id = :id',[// pegar os dados desse item especifico
-            'id'=>$id// receber da rota aqui oid
-        ]);
+        // $data = DB::select('SELECT * FROM tarefas WHERE id = :id',[// pegar os dados desse item especifico
+        //     'id'=>$id// receber da rota aqui oid
+        // ]);
 
-        if(count($data)>0){
+        $data = Tarefa::find($id);// sustitui a query abaixo (acha o proprio item)
+
+        // if(count($data)>0){ // nao vai ver se teve mais um item
+        if($data){// ele vai ver se teve o item
         return view('tarefas.edit', [
-            'data'=> $data[0]
+            'data'=> $data
         ]);
         }else{
             return redirect()->route('tarefas.list');
@@ -55,28 +66,43 @@ class TarefasController extends Controller
        
             $titulo = $request->input('titulo');
 
-           DB::select('SELECT * FROM tarefas WHERE id = :id',[// pegar os dados desse item especifico
-                'id'=>$id// receber da rota aqui oid
-            ]);
+        //    DB::select('UPDATE tarefas SET titulo = :titulo WHERE id  = :id',[// fazer a atualização
+        //         'id'=>$id,// receber da rota aqui oid
+        //         'titulo' =>$titulo
+        //     ]);
+
+                // $t = Tarefa::find($id);// achar o item
+                // $t->titulo = $titulo;// atualzar
+                // $t->save();// salvar
+
+                Tarefa::find($id)->update(['titulo'=>$titulo]); // outra forma de fazer
     
           
             return redirect()->route('tarefas.list');
           
     }
     public function del($id){
-        DB::delete('DELETE FROM tarefas WHERE id= :id', [
-           'id'=>$id
-        ]);
+        // DB::delete('DELETE FROM tarefas WHERE id= :id', [
+        //    'id'=>$id
+        // ]);
+
+        Tarefa::find($id)->delete();
         return redirect()->route('tarefas.list');
     }
     public function done($id){
         // opção 1 : select + update
         // opcao 2 : update matematico
 
-        DB::update('UPDATE tarefas SET resolvido = 1 - resolvido WHERE id = :id', [
-            'id'=>$id
-        ]);
+        // DB::update('UPDATE tarefas SET resolvido = 1 - resolvido WHERE id = :id', [
+        //     'id'=>$id
+        // ]);
 
+        $t = Tarefa::find($id);// vai achar o carinha
+        if($t){// se ele achou essa tarefa
+        $t->resolvido = 1- $t->resolvido;
+        $t->save();
+
+        }
         return redirect()->route('tarefas.list');
         //retorno
     }
